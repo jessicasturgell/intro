90059834cdfc094ae0a1f6faaab63d79246a4720
Jessica Sturgell <sturgell01@gmail.com>
2024-07-15 10:50:20 -0400

    commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.Write("Who would you like to say hello to? ");
+
+string name = System.Console.ReadLine();
+
+if (string.IsNullOrWhiteSpace(name))
+{
+    System.Console.WriteLine("Fine, don't say 'hello'!");
+}
+else
+{
+    System.Console.WriteLine($"Hello, {name}!");
+}
